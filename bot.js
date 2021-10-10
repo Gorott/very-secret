637c59c60d0e083bcf2d2748dcfe2213471330ab
Groot <67421407+Gorott@users.noreply.github.com>
2021-10-10 16:04:28 +0200

    Initial commit

---
+++ bot.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+const { Collection, Client, Discord } = require("discord.js");
+const fs = require("fs");
+const client = new Client({
+  disableEveryone: true,
+  intents: 32767,
+});
+
+const token = process.env.token;
+client.commands = new Collection();
+fs.readdir("./commands/", (err, files) => {
+  files.forEach((file) => {
+    let path = `./commands/${file}`;
+    fs.readdir(path, (err, files) => {
+      if (err) console.error(err);
+      let jsfile = files.filter((f) => f.split(".").pop() === "js");
+      if (jsfile.length <= 0) {
+        console.error(`Couldn't find commands in the ${file} category.`);
+        return;
+      }
+      jsfile.forEach((f, i) => {
+        let props = require(`./commands/${file}/${f}`);
+        props.category = file;
+        try {
+          client.commands.set(props.name, props);
+          if (props.alias)
+            props.alias.forEach((alias) => client.commands.set(alias, props));
+        } catch (err) {
+          if (err) console.error(err);
+        }
+      });
+    });
+  });
+});
+
+const eventFiles = fs.readdirSync("./events").filter((file) => file.endsWith(".js"));
+for (const file of eventFiles) {
+  require(`./events/${file}`)(client);
+}
+
+client.login(token);
+
+client.on("error", (e) => console.error);
+
+module.exports = { client };
---
+++ events/messageCreate.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const config = require("../config");
+const prefix = process.env.prefix;
+
+module.exports = (client) => {
+  client.on("messageCreate", async (message) => {
+    if (message.author.bot) return;
+    if (message.channel.type == "dm") return;
+    if (!message.content.startsWith(prefix)) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const cmd = args.shift().toLowerCase();
+    const command =
+      client.commands.get(cmd) ||
+      client.commands.find(
+        (command) => command.aliases && command.aliases.includes(cmd)
+      );
+    if (!command) return;
+
+    await command.run(message, args, client)?.catch((error) => {
+      console.error(error);
+    });
+  });
+};
